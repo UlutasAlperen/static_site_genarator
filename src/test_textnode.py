22585ecda024d1a075b34e6d60f536e8f9d176d7
UlutasAlperen <ulutas.alperen@outlook.com>
2025-04-17 18:48:33 +0300

    update tests files

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,3 +1,4 @@
+from typing_extensions import Text
 import unittest
 from textnode import TextNode,TextType
 
@@ -6,7 +7,15 @@ class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
+        node3 = TextNode("This is not a node", TextType.BOLD)
+        node4 = TextNode("This is not a node", TextType.TEXT)
+        node5 = TextNode("This is not a node", TextType.TEXT)
         self.assertEqual(node, node2)
+        self.assertNotEqual(node2,node3)
+        self.assertNotEqual(node,node5)
+        self.assertEqual(node5, node4)
+
+
 
 
 if __name__ == "__main__":
