c9ad7cdf1b69acea8eec1b5e27442e3e616d5ec7
UlutasAlperen <ulutas.alperen@outlook.com>
2025-04-30 16:11:13 +0300

    update htmlnode.py

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -167,9 +167,10 @@ def split_nodes_link(old_nodes):
 
 
 def text_to_textnodes(text):
-    for delimeter in ["**","`","_"]
-
-
-
-
-
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    return nodes
