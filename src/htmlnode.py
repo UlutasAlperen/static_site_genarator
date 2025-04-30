20fd17d5f791494a2cd82d3227b56ecc3336dc0d
UlutasAlperen <ulutas.alperen@outlook.com>
2025-04-30 13:30:33 +0300

    update htmlnode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -107,8 +107,69 @@ def split_nodes_delimiter(old_nodes,delimiter,text_type):
 def extract_markdown_images(text):
     return re.findall(r'!\[([^\]]+)\]\(([^)]+)\)',text)
 
+
+
 def extract_markdown_links(text):
     return re.findall(r'(?<!!)\[([^\]]+)\]\(([^)]+)\)',text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        matches = extract_markdown_images(node.text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for alt, url in matches:
+            full_syntax = f"![{alt}]({url})"
+            parts = text.split(full_syntax, 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(alt, TextType.IMAGE, url))
+            text = parts[1]
+        if text:
+            new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return [n for n in new_nodes if n.text != ""]
+
+
+def split_nodes_link(old_nodes):
+    new_nodes = []
+
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+
+        matches = extract_markdown_links(node.text)
+        if not matches:
+            new_nodes.append(node)
+            continue
+
+        text = node.text
+        for label, url in matches:
+            full_syntax = f"[{label}]({url})"
+            parts = text.split(full_syntax, 1)
+            if parts[0]:
+                new_nodes.append(TextNode(parts[0], TextType.TEXT))
+            new_nodes.append(TextNode(label, TextType.LINK, url))
+            text = parts[1]
+        if text:
+            new_nodes.append(TextNode(text, TextType.TEXT))
+
+    return [n for n in new_nodes if n.text != ""]
+
+
+def text_to_textnodes(text):
+    for delimeter in ["**","`","_"]
+
+
 
 
 
